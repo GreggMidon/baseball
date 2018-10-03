1aae2e749ce9b84cbb03932217992c4f94c88116
Gregg Midon <3g.sr.yo@gmail.com>
2018-10-02 21:46:21 -0400

    Major refactoring for performance.

--- loaders/event_ldr.py
+++ loaders/event_ldr.py
@@ -4,6 +4,7 @@ Descr......: loads Event records into database
 Author.....: Gregg Midon
 Date.......: 8/26/2018
 '''
+import datetime
 from db.db_funcs import getconnection, gameindex, insertinfo, insertstart, insertplay, insertsub, insertdata
 
 
@@ -12,55 +13,101 @@ def load(file, season):
     # connect to database
     conn = getconnection()
 
-    # open file for source data
+    # source file
     tfil = open(file, 'r')
 
     # iterate through the file
-    lnum = 0
     firsttime = True
+    lnum = 0;
 
     for line in tfil:
         line = line[:-1]
         recordList = line.split(',')
         rectyp = recordList[0]
-        lnum += 1
 
         if rectyp == 'id':
             id = recordList[1]
-            lnum = 0
+            #print(' -> ' + id)
             index = gameindex(conn)
 
             if firsttime:
                 firsttime = False
             else:
-                insertinfo(conn, index, season, info)
+                insertinfo(conn, _info)
+                insertstart(conn, _start)
+                insertsub(conn, _sub)
+                insertdata(conn, _data)
+                insertplay(conn, _play)
 
             # setup for next game
-            info = dict()
-            info['id'] = id
+            _info = dict()
+            _info['id'] = id
+            _info['index'] = index
+            _info['season'] = season
+            _start = []
+            _play = []
+            _sub = []
+            _data = []
+            lnum = 0
 
         elif rectyp == 'info':
             rkey = recordList[1]
             rval = recordList[2]
-            info[rkey] = rval
+            _info[rkey] = rval
 
         elif rectyp == 'start':
-            # insert start record
-            insertstart(conn, index, lnum, recordList)
+            d = dict()
+            d['game_ndx'] = index
+            d['player_id'] = recordList[1]
+            d['player_nm'] = recordList[2]
+            d['visit_home'] = recordList[3]
+            d['order'] = recordList[4]
+            d['position'] = recordList[5]
+            d['entry_dt'] = datetime.datetime.now()
+            _start.append(d)
 
         elif rectyp == 'play':
-            # insert play record
-            insertplay(conn, index, lnum, recordList)
+            lnum += 1
+            d = dict()
+            d['game_ndx'] = index
+            d['seq_num'] = lnum
+            d['inning'] = recordList[1]
+            d['visit_home'] = recordList[2]
+            d['player_id'] = recordList[3]
+            d['pitch_cnt'] = recordList[4]
+            d['pitches'] = recordList[5]
+            d['event_str'] = recordList[6]
+            d['entry_dt'] = datetime.datetime.now()
+            _play.append(d)
+
 
         elif rectyp == 'sub':
-            # insert sub record
-            insertsub(conn, index, lnum, recordList)
+            lnum += 1
+            d = dict()
+            d['game_ndx'] = index
+            d['seq_num'] = lnum
+            d['player_id'] = recordList[1]
+            d['player_nm'] = recordList[2]
+            d['visit_home'] = recordList[3]
+            d['order'] = recordList[4]
+            d['position'] = recordList[5]
+            d['entry_dt'] = datetime.datetime.now()
+            _sub.append(d)
 
         elif rectyp == 'data':
-            # insert data record
-            insertdata(conn, index, lnum, recordList)
+            d = dict()
+            d['game_ndx'] = index
+            d['data_typ'] = recordList[1]
+            d['player_id'] = recordList[2]
+            d['smint_val'] = recordList[3]
+            d['entry_dt'] = datetime.datetime.now()
+            _data.append(d)
 
-    conn.commit()
+    insertinfo(conn, _info)
+    insertstart(conn, _start)
+    insertsub(conn, _sub)
+    insertdata(conn, _data)
+    insertplay(conn, _play)
 
     # close communication with the database
     conn.close()
