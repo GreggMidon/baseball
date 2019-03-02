3af38fb6e3665ff8c66d3861641134a7cb25503a
Gregg Midon <3g.sr.yo@gmail.com>
2019-03-01 17:03:16 -0700

    Naming convention refactoring.

--- fileio/filesys.py
+++ fileio/filesys.py
@@ -2,37 +2,37 @@ from pathlib import Path
 from util.common import *
 
 
-def getrosterfiles(season):
-    return getfiles(season, rosterfiles)
+def get_roster_files(season):
+    return get_files(season, rosterfiles)
 
 
-def geteventfiles(season):
-    return getfiles(season, all_events)
+def get_event_files(season):
+    return get_files(season, all_events)
 
 
-def getteamfile(season):
+def get_team_file(season):
     return rootdir + str(season) + '/TEAM' + str(season)
 
 
-def getamerleagevents(season) :
-    return getfiles(season, al_events)
+def get_al_events(season) :
+    return get_files(season, al_events)
 
 
-def getnatlleagevents(season):
-    return getfiles(season, nl_events)
+def get_nl_events(season):
+    return get_files(season, nl_events)
 
 
-def getallseasonfiles(season):
-    return getfiles(season, all_files)
+def get_all_season_files(season):
+    return get_files(season, all_files)
 
 
-def geteventfilesstaged():
+def get_event_files_staged():
     workingdir = stagedir
     p = Path(workingdir)
     return list(p.glob(all_files))
 
 
-def getfiles(season, fglob):
+def get_files(season, fglob):
     workingdir = rootdir + str(season) + '/'
     p = Path(workingdir)
     return list(p.glob(fglob))
